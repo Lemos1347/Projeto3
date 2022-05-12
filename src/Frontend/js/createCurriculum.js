81acd77642243092abb567812b73d9ea8d7e81bf
Pedro Hagge Baptista <Pedro.Baptista@sou.inteli.edu.br>
2022-05-12 17:43:46 -0300

    atualização card cadastrar currículo

--- src/Frontend/js/createCurriculum.js
+++ src/Frontend/js/createCurriculum.js
@@ -1,4 +1,65 @@
 
+let cards = [
+    // {
+    //     "message": "teste",
+    //     "id": 1
+    // },
+    // {
+    //     "message": "teste1",
+    //     "id": 2
+    // }
+]
+
+
+function addCards(message) {
+    let lastCard = cards.slice(-1)
+
+    if (lastCard == '') {
+        cards.push({'message': message, 'id': 1})
+    } else {
+        var lastId = lastCard[0].id
+        var id = lastId + 1
+        cards.push({'message': message, 'id': id})
+    }
+    renderCards()
+    console.log(cards)
+}
+
+function deleteCard(id) {
+    var cardDeleted = cards.filter((card) => {
+        card.id != id        
+    })
+
+    cards = cardDeleted
+
+    renderCards()
+}
+
+function renderCards() {
+    cards.map((card) => {
+        document.getElementById('skillCard').innerHTML += `
+        <div class="balao-skill d-flex">
+            <p>${card.message}</p><button type = 'button' onclick = deleteCard(${card.id})>x</button>
+        </div>
+        `
+    })
+}
+
+addCards('Testeeeee')
+addCards('Testeeeee2')
+
+// function updateCards() {
+//     cards.map((card) => {
+//         document.getElementById('skillCard').innerHTML += `
+//         <div class="balao-skill d-flex">
+//             <p>${card.message}</p><button>x</button>
+//         </div>
+//         `
+//     })
+//     console.log(cards)
+// }
+
+// cards.push = function() { Array.prototype.push.apply(this, arguments);  updateCards();};
 
 function verifyInfos() {
 
@@ -21,3 +82,4 @@ function verifyInfos() {
 function generateRandomNumber() {
     return Math.floor(Math.random() * 300)
 }
+
