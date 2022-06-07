f0054ee2f46a1c9b69752a34531b9680c828527f
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2022-06-07 09:59:16 -0300

    End point do match completo!
    Co-Authored-By: Pedro Hagge Baptista <99206621+PedroHaggeBaptista@users.noreply.github.com>

--- src/Backend/controllers/Match.js
+++ src/Backend/controllers/Match.js
@@ -1,20 +1,34 @@
+//Classe exportada advinda de outro arquivo 
+const matchGenerate = require('../services/Match')
+
+//Controller para obter a porcentagem de match da usuária com a vaga 
 const match = (req, res) => {
-    // const teste1 = {
-    //     skill1: 'teste',
-    //     skill2: 'teste2',
-    //     skill3: 'teste3'
-    // }
-    let teste1 = 'skill1, skill2, skill3'
-    let { softSkillUser, softSkillOffer } = req.body;
+    let { userSoft, offerSoft, userHard, offerHard } = req.body;
+    //Substitui todos os elementos indesejáveis e formata as variáveis em um array
+    userSoft = userSoft.replaceAll('[', "")
+    userSoft = userSoft.replaceAll(']', "")
+    offerSoft = offerSoft.replaceAll('[', "")
+    offerSoft = offerSoft.replaceAll(']', "")
+    userHard = userHard.replaceAll('[', "")
+    userHard = userHard.replaceAll(']', "")
+    offerHard = offerHard.replaceAll('[', "")
+    offerHard = offerHard.replaceAll(']', "")
+    //Remove as vígulas de cada elemento de cada array
+    userSoft = userSoft.split(", ")
+    userHard = userHard.split(", ")
+    offerSoft = offerSoft.split(", ")
+    offerHard = offerHard.split(", ")
 
-    softSkillOffer = JSON.parse(String(softSkillOffer))
-    softSkillUser = JSON.parse(String(softSkillUser))
+    //Instancia a classe match exportada
+    var matchCreate = new matchGenerate.Match(userSoft, offerSoft, userHard, offerHard);
 
-    var teste = teste1.split(" ")
+    //Executa os métodos da classe previamente exportada
+    var resulSoft = matchCreate.matchSoft();
+    var resulHard = matchCreate.matchHard();
+    //Calcula o resultadado final em decorrência dos resultados obtidos nas funções acima
+    const resulFinal = (resulSoft + resulHard) / 2
 
-    console.log(softSkillOffer)
-    console.log(softSkillUser)
-    console.log(teste)
+    res.status(200).send({resulFinal})
 }
 
 module.exports = {
